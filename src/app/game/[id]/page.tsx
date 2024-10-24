80b83b8c33f80f71b5ee8d514f39d08c0e8293a4
imneli <contatodoneli@gmail.com>
2024-10-24 09:15:57 -0300

    caches and games

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -3,20 +3,31 @@ import { GameProps } from "@/utils/types/game"
 import Image from "next/image"
 import { redirect } from "next/navigation"
 import { Label } from "./components/label"
+import GameCard from "@/components/GameCard"
 
 
 async function getData( id: string ) { 
     try {
-        const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`)
+        const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`, { cache: "no-store" })
         return res.json()
     } catch(err) {
         throw new Error("Failed do fetch")
     }
 }
 
+async function getGameSort(){
+    try {
+        const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, { cache: "no-store" })
+        return res.json()
+    } catch(err) {
+        throw new Error("Failed")
+    }
+}
+
 export default async function Game({ params: { id } }: { params: { id: string } }) {
     
     const data: GameProps = await getData(id)
+    const sortedGame: GameProps = await getGameSort()
 
     if (!data) {
         redirect("/")
@@ -39,11 +50,27 @@ export default async function Game({ params: { id } }: { params: { id: string } 
                 <h1 className="font-bold text-xl my-4">{data.title}</h1>
                 <p>{data.description}</p>
 
+                <h2 className="font-bold text-lg mt-7 mb-2">Plataformas</h2>
+                <div className="flex gap-2 flex-wrap">
+                    {data.platforms.map((item) => (
+                        <Label name={item} key={item} />
+                    ))}
+                </div>
+
                 <h2 className="font-bold text-lg mt-7 mb-2">Categorias</h2>
                 <div className="flex gap-2 flex-wrap">
                     {data.categories.map((item) => (
                         <Label name={item} key={item} />
                     ))}
+                </div>
+
+                <p className="mt-5"><strong>Data de lançamento:</strong> {data.release}</p>
+
+                <h2 className="font-bold text-lg mt-7 mb-2">Jogo Recomendado: </h2>
+                <div className="flex">
+                    <div className="flex-grow">
+                        <GameCard data={sortedGame} />
+                    </div>
                 </div>
             </Container>
         </main>
